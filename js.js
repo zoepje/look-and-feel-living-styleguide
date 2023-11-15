00fa329847e7df6ae6575d9d52062b2a32a62ebe
Seijno <seijnovannieuwkoop@gmail.com>
2023-11-15 11:28:25 +0100

    menu

---
+++ js.js
@@ -0,0 +1,7 @@
+function openMenu() {
+    document.getElementById("sideMenu").style.width = "30vw";
+  }
+  
+  function closeMenu() {
+    document.getElementById("sideMenu").style.width = "0";
+  }
